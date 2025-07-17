1b77fc38b8055f41cbfb06fc42a134ea48761b56
jonochan <jonathanchan.jccc@gmail.com>
2025-07-17 16:23:36 +0100

    lighting for day/night

--- shaders/basic_frag.glsl
+++ shaders/basic_frag.glsl
@@ -4,13 +4,16 @@
 layout(location = 0) out vec4 outColor0;
 
 uniform sampler2D gtexture; //texture sampler
+uniform sampler2D lightmap; //lightmap sampler
 
 in vec2 texCoord;
 in vec3 foliageColor;
+in vec2 lightMapCoords; //lightmap coordinates
 
 void main() {
+    vec3 lightColor = texture(lightmap, lightMapCoords).rgb;
     vec4 outputColorData = texture(gtexture, texCoord);
-    vec3 outputColor = outputColorData.rgb * foliageColor; //apply foliage color to the texture color
+    vec3 outputColor = outputColorData.rgb * foliageColor * lightColor; //apply foliage color to the texture color
     float transparency = outputColorData.a; //use the alpha from the texture
     if (transparency < 0.1) {
         discard; //discard fragments with low alpha
--- shaders/gbuffers_basic.vsh
+++ shaders/gbuffers_basic.vsh
@@ -1,18 +1,1 @@
-#version 460
-
-uniform mat4 modelViewMatrix;
-uniform mat4 modelViewMatrixInverse;
-uniform mat4 projectionMatrix;
-
-in vec3 vaPosition; //vertex position, xyz
-in vec2 vaUV0;
-in vec4 vaColor;
-
-out vec2 texCoord;
-out vec3 foliageColor;
-
-
-void main() {
-    texCoord = vaUV0; //pass through UV coordinates to fragment shader
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(vaPosition, 1.0); //convert from modelspace to clip space
-}
+#include basic_vert.glsl
